eebb66c0e0d521bdb415fff3917fba65b1130ef3
Kartik Kumar <kartikkumarxx4@gmail.com>
2026-09-06 20:45:51 +0530

    Day04: practice some questions

---
+++ DSA IN JAVA/Day04/Practice.java
@@ -0,0 +1,22 @@
+public class Practice {
+    static void main() {
+//        Write a Java program that prints: 1-10
+//        for(int i=1; i<=10; i++){
+//            System.out.println(i);
+//        }
+
+//        Write a program that prints all even numbers from 1 to 20
+//        for(int i=1; i<=20; i++){
+//            if(i % 2 ==0){
+//                System.out.println(i);
+//            }
+//        }
+
+//        Write a program that calculates the sum of numbers from 1 to 10.
+        int sum=0;
+        for(int i=1; i<=10; i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+}

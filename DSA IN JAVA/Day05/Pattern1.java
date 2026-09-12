69f2eeb1b139c30d82df63e30264023cc293506d
Kartik Kumar <kartikkumarxx4@gmail.com>
2026-09-12 22:36:47 +0530

    Day05: learned how to solve patterns

---
+++ DSA IN JAVA/Day05/Pattern1.java
@@ -0,0 +1,30 @@
+public class Pattern1 {
+    static void main() {
+//         PRINT
+//                 ****
+//                 ****
+//                 ****
+//                 ****
+
+//        int n = 4;
+//        for(int row =1; row<=n; row++){
+//            for(int col=1; col<=n; col++){
+//                System.out.print("*  ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        -------PATTERN 2 RIGHT A T-------
+        int n =5;
+        for(int row=1; row<=n; row++){
+            for(int j=1; j<=row; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
